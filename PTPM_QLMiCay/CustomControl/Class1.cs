b394fa53ebec94d9f525d9d7263142aefe6f2fe7
Thành Tân <94952967+thanhtan34@users.noreply.github.com>
2023-09-12 02:36:05 +0700

    Update_PJ_01

--- PTPM_QLMiCay/CustomControl/Class1.cs
+++ PTPM_QLMiCay/CustomControl/Class1.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomControls
+namespace CustomControl
 {
-    public class TestCustomsFild
+    public class Class1
     {
-
     }
 }
