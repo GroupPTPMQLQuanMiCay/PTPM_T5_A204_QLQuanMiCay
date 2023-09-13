e235e92d4651985b58f873990b7e2bbfce084819
Thành Tân <94952967+thanhtan34@users.noreply.github.com>
2023-09-13 15:52:14 +0700

    update FormEntity

--- PTPM_QLMiCay/CustomControl/FormCustom/FormEntity.cs
+++ PTPM_QLMiCay/CustomControl/FormCustom/FormEntity.cs
@@ -10,11 +10,61 @@ namespace CustomControl.FormCustom
 {
     public class FormEntity : Form
     {
+        public Point mouseLocation;
+
         public FormEntity() 
         { 
             this.BackColor = Color.White;
             this.StartPosition = FormStartPosition.CenterScreen;
             this.FormBorderStyle = FormBorderStyle.None;
+            this.MouseDown += FormEntity_MouseDown;
+            this.MouseMove += FormEntity_MouseMove;
         }
+
+        private void FormEntity_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void FormEntity_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePose = Control.MousePosition;
+                mousePose.Offset(mouseLocation.X, mouseLocation.Y);
+                Location = mousePose;
+            }
+        }
+
+        //private void InitializeComponent()
+        //{
+        //    this.SuspendLayout();
+        //    // 
+        //    // FormEntity
+        //    // 
+        //    this.ClientSize = new System.Drawing.Size(284, 261);
+        //    this.Name = "FormEntity";
+        //    this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouse_Down);
+        //    this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mouse_Move);
+        //    this.ResumeLayout(false);
+
+        //}
+
+        //private void mouse_Down(object sender, MouseEventArgs e)
+        //{
+        //    mouseLocation = new Point(-e.X, -e.Y);
+
+        //}
+
+        //private void mouse_Move(object sender, MouseEventArgs e)
+        //{
+        //    if (e.Button == MouseButtons.Left)
+        //    {
+        //        Point mousePose = Control.MousePosition;
+        //        mousePose.Offset(mouseLocation.X, mouseLocation.Y);
+        //        Location = mousePose;
+        //    }
+
+        //}
     }
 }
--- PTPM_QLMiCay/PTPM_QLMiCay/GUI/fDangNhap.Designer.cs
+++ PTPM_QLMiCay/PTPM_QLMiCay/GUI/fDangNhap.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // fDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1015, 483);
+            this.Name = "fDangNhap";
             this.Text = "fDangNhap";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
